948236195dc864d79626c0bfa5fd9e0a18fbef8b
ibnuck <masitoh7564@gmail.com>
2026-05-13 17:11:33 +0700

    feat: add comment

--- FrostDive/ECS/Entities/submarine.swift
+++ FrostDive/ECS/Entities/submarine.swift
@@ -29,3 +29,21 @@ class submarineEntity: GKEntity {
     }
     required init?(coder aDecoder: NSCoder) { fatalError() }
 }
+
+
+//var entities = [GKEntity]()
+//
+//func spawnSubmarine(){
+//    let submarine = submarineEntity(imageName: "submarine", size: CGSize(width: 120, height: 82))
+//    
+//    if let spriteComponent = submarine.component(ofType: spriteComponent.self) {
+//        let node = spriteComponent.node
+//        node.position = CGPoint(x: size.width * 0.2, y: size.height / 2)
+//        addChild(node)
+//    }
+//    self.entities.append(submarine)
+//}
+//
+//override func didMove(to view: SKView) {
+//    spawnSubmarine()
+//}
