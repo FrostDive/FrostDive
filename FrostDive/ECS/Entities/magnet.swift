48339c1271b4ce1f673f7bdbcc870747e834da88
ibnuck <masitoh7564@gmail.com>
2026-05-13 21:34:26 +0700

    feat: magnet entity

--- FrostDive/ECS/Entities/magnet.swift
+++ FrostDive/ECS/Entities/magnet.swift
@@ -8,3 +8,23 @@
 import SpriteKit
 import GameplayKit
 
+class magnetEntity: GKEntity {
+    init(imageName: String, size: CGSize) {
+        super.init()
+        
+        let texture: SKTexture = .init(imageNamed: imageName) // nanti cara pakenya gini
+        let spriteComponent = spriteComponent(texture: texture, size: size)
+        addComponent(spriteComponent)
+        
+        let node = spriteComponent.node
+        node.physicsBody = SKPhysicsBody(texture: texture, size: size)
+        node.physicsBody?.isDynamic = false
+        node.physicsBody?.affectedByGravity = false
+        node.physicsBody?.allowsRotation = false
+        node.physicsBody?.categoryBitMask = physicsCategory.obstacle
+        node.physicsBody?.contactTestBitMask = physicsCategory.submarine
+        node.physicsBody?.collisionBitMask = physicsCategory.none
+        
+    }
+    required init?(coder aDecoder: NSCoder) { fatalError() }
+}
