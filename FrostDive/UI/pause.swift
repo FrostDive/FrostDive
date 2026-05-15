7afeac850c0654ec5a33c771a809d535c361241d
febeflo <steffanyflorence@gmail.com>
2026-05-15 16:40:02 +0700

    feat: pause screen

--- FrostDive/UI/pause.swift
+++ FrostDive/UI/pause.swift
@@ -5,3 +5,38 @@
 //  Created by Steffany Florence on 13/05/26.
 //
 
+import SwiftUI
+
+struct pause: View {
+    @ObservedObject var gameState: gameState
+    
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.4).edgesIgnoringSafeArea(.all)
+            VStack(spacing: 20) {
+                Text("PAUSE")
+                    .font(.custom("milner", size: 55, relativeTo: .title))
+                    .foregroundStyle(Color(hex: "#D8EDF5"))
+                
+                Button(action: {
+                    gameState.isPaused = false
+                }) {
+                    Image("continuePause")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 220)
+                }
+                
+                Button(action: {
+                    gameState.isPaused = false
+                    gameState.currentScreen = .home
+                }) {
+                    Image("homePause")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 220)
+                }
+            }
+        }
+    }
+}
