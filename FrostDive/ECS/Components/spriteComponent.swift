6880ce39669b511a2d510f983e5f0ad7d8916a0a
thereuwuw <theressathebez.15@gmail.com>
2026-05-15 18:15:17 +0700

    Merge sound

--- FrostDive/ECS/Components/spriteComponent.swift
+++ FrostDive/ECS/Components/spriteComponent.swift
@@ -5,22 +5,33 @@
 //  Created by Steffany Florence on 13/05/26.
 //
 
-import SpriteKit
 import GameplayKit
+import SpriteKit
 
 class spriteComponent: GKComponent {
-    
+
     let node: SKSpriteNode
-    
-    init (texture: SKTexture, size: CGSize) {
+
+    let normalTexture = SKTexture(imageNamed: "submarine1")
+    let magnetTexture = SKTexture(imageNamed: "submarineMagnet1")
+
+    init(texture: SKTexture, size: CGSize) {
         self.node = SKSpriteNode(texture: texture, color: .clear, size: size)
         super.init()
     }
-    
-    func updateTexture(_ texture:SKTexture)  {
+
+    func updateTexture(_ texture: SKTexture) {
         node.texture = texture
     }
-    
+
+    func setNormal() {
+        node.texture = normalTexture
+    }
+
+    func setMagnet() {
+        node.texture = magnetTexture
+    }
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
