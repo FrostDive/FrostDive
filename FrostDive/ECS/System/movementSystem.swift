aaa9d72ef958a531f0010f8d4b8c2077ad4bfc0e
Ibnu Taufick Ahraza <103435924+Ibnuck@users.noreply.github.com>
2026-05-15 16:57:28 +0700

    Feat/magnet (#16)
    * feat: spawning magnet, and magnet delay spawning
    
    * feat: magnet logic for pulling trash
    
    * feat: another magnet pulling logic
    
    * feat: trash magnet pulling logic into the submarine is working, but magnet collision is not making yet
    
    * feat: colision with magnet to activate magnet pull working

--- FrostDive/ECS/System/movementSystem.swift
+++ FrostDive/ECS/System/movementSystem.swift
@@ -10,7 +10,10 @@ import GameplayKit
 
 class MovementSystem: GKComponentSystem<movementComponent> {
     var sceneSize: CGSize
-    let totalBackgrounds: CGFloat = 15 // Sesuai asetmu
+    let totalBackgrounds: CGFloat = 15 // Sesuai aset background
+    
+    var isMagnetActive: Bool = false
+    var submarinePosition: CGPoint?
     
     init(sceneSize: CGSize) {
         self.sceneSize = sceneSize
@@ -33,6 +36,25 @@ class MovementSystem: GKComponentSystem<movementComponent> {
                     posComp.position.x += sceneSize.width * totalBackgrounds
                 }
             }
+            
+            if isMagnetActive, entity is trashEntity, let target = submarinePosition {
+                // Hitung jarak x dan y antara sampah dan kapal selam
+                let dx = target.x - posComp.position.x
+                let dy = target.y - posComp.position.y
+                
+                // Hitung total jarak miring (Pythagoras)
+                let distance = sqrt(dx * dx + dy * dy)
+                
+                // Jika jaraknya masih lebih dari 5 pixel, tarik perlahan
+                if distance > 5.0 {
+                    let magnetPullSpeed: CGFloat = 8.0 // Semakin besar, sedotannya makin kencang
+                    
+                    // Rumus vektor: Arahkan ke target lalu kalikan kecepatan tarik
+                    posComp.position.x += (dx / distance) * magnetPullSpeed
+                    posComp.position.y += (dy / distance) * magnetPullSpeed
+                }
+            }
         }
     }
 }
+
