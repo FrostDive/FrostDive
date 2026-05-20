1a19f30ed82f3c57f9244e80025db1ee99549a7f
thereuwuw <theressathebez.15@gmail.com>
2026-05-20 16:13:03 +0700

    Fix: Home scene

--- FrostDive/ECS/System/movementSystem.swift
+++ FrostDive/ECS/System/movementSystem.swift
@@ -10,7 +10,7 @@ import GameplayKit
 class MovementSystem: GKComponentSystem<movementComponent> {
     var sceneSize: CGSize
     let totalBackgrounds: CGFloat = 15
-
+    
     var isMagnetActive: Bool = false
     var submarinePosition: CGPoint?
     var speedMultiplier: CGFloat = 1.0
@@ -19,34 +19,28 @@ class MovementSystem: GKComponentSystem<movementComponent> {
         self.sceneSize = sceneSize
         super.init(componentClass: movementComponent.self)
     }
-
+    
     override func update(deltaTime seconds: TimeInterval) {
         for component in components {
             guard let entity = component.entity,
-                let posComp = entity.component(ofType: positionComponent.self)
+                  let posComp = entity.component(ofType: positionComponent.self)
             else { continue }
-
-            posComp.position.x -= component.speed
-
-            if entity is trashEntity {
-
-                let waveSpeed: CGFloat = 1.2
-                let waveHeight: CGFloat = 0.4
-
-                let wave =
-                    cos(CGFloat(CACurrentMediaTime()) * waveSpeed
-                    + component.bobbingOffset)
-                    * waveHeight
-
-                posComp.position.y += wave
-            }
-
-                  let posComp = entity.component(ofType: positionComponent.self) else { continue }
             
-            // Gerakkan ke kiri
             posComp.position.x -= (component.speed * speedMultiplier)
             
-            // Logika Looping untuk 15 Background
+            if entity is trashEntity {
+                
+                let waveSpeed: CGFloat = 1.2
+                let waveHeight: CGFloat = 0.4
+                
+                let wave =
+                cos(CGFloat(CACurrentMediaTime()) * waveSpeed
+                    + component.bobbingOffset)
+                * waveHeight
+                
+                posComp.position.y += wave
+            }
+        
             if entity is backgroundEntity {
                 if posComp.position.x <= -sceneSize.width {
                     // Pindahkan ke paling belakang dari rangkaian 15 gambar
@@ -61,10 +55,10 @@ class MovementSystem: GKComponentSystem<movementComponent> {
                 // Hitung jarak x dan y antara sampah dan kapal selam
                 let dx = target.x - posComp.position.x
                 let dy = target.y - posComp.position.y
-
+                
                 // Hitung total jarak miring (Pythagoras)
                 let distance = sqrt(dx * dx + dy * dy)
-
+                
                 // Jika jaraknya masih lebih dari 5 pixel, tarik perlahan
                 if distance < 300.0 {
                     isBeingPulled = true
@@ -77,23 +71,23 @@ class MovementSystem: GKComponentSystem<movementComponent> {
             }
             
             if !isBeingPulled, let animComp = entity.component(ofType: animationComponent.self) {
-                            
-                            // Ambil titik Y awal saat objek pertama kali muncul
-                            if !animComp.isInitialized {
-                                animComp.startY = posComp.position.y
-                                // Beri nilai acak pada timePassed agar setiap objek mengambang tidak serentak (sync)
-                                animComp.timePassed = Double.random(in: 0...Double.pi * 2)
-                                animComp.isInitialized = true
-                            }
-                            
-                            // Majukan waktu
+                
+                // Ambil titik Y awal saat objek pertama kali muncul
+                if !animComp.isInitialized {
+                    animComp.startY = posComp.position.y
+                    // Beri nilai acak pada timePassed agar setiap objek mengambang tidak serentak (sync)
+                    animComp.timePassed = Double.random(in: 0...Double.pi * 2)
+                    animComp.isInitialized = true
+                }
+                
+                // Majukan waktu
                 animComp.timePassed += seconds * Double(animComp.animationSpeed)
-                            
-                            // Hitung posisi Y baru menggunakan Gelombang Sinus
-                            // sin() menghasilkan kurva mulus dari -1 hingga 1.
-                            let waveOffset = CGFloat(sin(animComp.timePassed)) * animComp.animationDistance
-                            posComp.position.y = animComp.startY + waveOffset
-                        }
+                
+                // Hitung posisi Y baru menggunakan Gelombang Sinus
+                // sin() menghasilkan kurva mulus dari -1 hingga 1.
+                let waveOffset = CGFloat(sin(animComp.timePassed)) * animComp.animationDistance
+                posComp.position.y = animComp.startY + waveOffset
+            }
         }
     }
 }
